a858210e57c6ae4fbe3503870fccbf022781a04e
Andrew Ray <delvarworld@gmail.com>
2016-10-10 17:20:52 -0700

    ding ding

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ if( !exists ) {
                 title VARCHAR(255),
                 includesFQs INTEGER,
                 fullText VARCHAR(255),
+                year INT,
+                month INT,
                 conclusions TEXT,
                 created_at TIMESTAMP NULL,
                 updated_at TIMESTAMP NULL
@@ -71,9 +73,9 @@ if( !exists ) {
         `);
 
         db.run( `
-            CREATE TABLE IF NOT EXISTS study_tags (
+            CREATE TABLE IF NOT EXISTS study_keywords (
                 id INTEGER PRIMARY KEY,
-                tag_id INTEGER REFERENCES keywords (id) ON DELETE SET NULL ON UPDATE CASCADE,
+                keyword_id INTEGER REFERENCES keywords (id) ON DELETE SET NULL ON UPDATE CASCADE,
                 study_id INTEGER REFERENCES studies (id) ON DELETE SET NULL ON UPDATE CASCADE
             )
         `);
@@ -119,6 +121,8 @@ app.post( '/add', addUploads, function( req, res ) {
     console.log( req.body, req.files );
 
     const title = req.body.title;
+    const month = req.body.month;
+    const year = req.body.year;
     const includesFqs = req.body.includesFqs;
     const conclusions = req.body.conclusions;
     const fullText = ( req.files && req.files[ 0 ] ) || req.body.fullText;
@@ -140,7 +144,7 @@ app.post( '/add', addUploads, function( req, res ) {
                 throw new Error( 'A study with this title has already been indexed' );
             }
 
-        // Find any existing author ids
+        // Find any existing author ids, building { name: id } object
         }).then( function() {
 
             return knex.select( 'name', 'id' )
@@ -157,10 +161,12 @@ app.post( '/add', addUploads, function( req, res ) {
         // Create any new authors
         }).then( function( existingAuthors ) {
 
+            // Find author names that aren't in the db already
             const newAuthors = authors.filter( function( name ) {
                 return !( name in existingAuthors );
             });
 
+            // Create them all
             return Promise.all( newAuthors.map( function( name ) {
                 return knex.insert({ name: name })
                     .returning([ 'name', 'id' ])
@@ -171,7 +177,8 @@ app.post( '/add', addUploads, function( req, res ) {
                             id: row[ 0 ],
                         };
                     });
-            // Build the full list of all authors
+            // Build the full list of all authors, merging the newly inserted
+            // ones with the existing list
             }) ).then( function( insertedAuthors ) {
                 return Object.assign(
                     {},
@@ -183,23 +190,162 @@ app.post( '/add', addUploads, function( req, res ) {
                 );
             });
 
+        // Find any existing keyword ids, building { name: id } object
         }).then( function( authors ) {
 
-            console.log('authors',authors);
+            return knex.select( 'name', 'id' )
+                .from( 'keywords' )
+                .whereIn( 'name', keywords )
+                .then( function( rows ) {
+                    return rows.reduce( function( memo, row ) {
+                        var newRow = {};
+                        newRow[ row.name ] = row.id;
+                        return Object.assign( {}, memo, newRow );
+                    }, {} )
+                }).then( function( existingKeywords ) {
+                    return {
+                        existingKeywords: existingKeywords,
+                        authors: authors,
+                    };
+                });
+
+        // Create any new keywords
+        }).then( function( continuation ) {
+
+            const existingKeywords = continuation.existingKeywords;
+
+            // Find author names that aren't in the db already
+            const newKeywords = keywords.filter( function( name ) {
+                return !( name in existingKeywords );
+            });
+
+            // Create them all
+            return Promise.all( newKeywords.map( function( name ) {
+                return knex.insert({ name: name })
+                    .returning([ 'name', 'id' ])
+                    .into( 'keywords' )
+                    .then( function( row ) {
+                        return {
+                            name: name,
+                            id: row[ 0 ],
+                        };
+                    });
+            // Build the full list of all authors, merging the newly inserted
+            // ones with the existing list
+            }) ).then( function( insertedKeywords ) {
+                return Object.assign(
+                    {},
+                    insertedKeywords.reduce( function( memo, author ) {
+                        memo[ author.name ] = author.id;
+                        return memo;
+                    }, {} ),
+                    existingKeywords
+                );
+            }).then( function( keywords ) {
+                return Object.assign( {}, continuation, {
+                    keywords: keywords,
+                });
+            });
+
+        // Create the actual study, get the id and pass authors
+        }).then( function( continuation ) {
+
             return knex.insert({
                 title: title,
                 includesFqs: includesFqs ? 1 : 0,
                 fullText: fullText,
+                month: month,
+                year: year,
                 conclusions: conclusions
-            }).into( 'studies' ).returning( 'id' );
+            }).into( 'studies' )
+                .returning( 'id' )
+                .then(function( studyRow ) {
 
-        }).then( function() {
+                    return Object.assign( {}, continuation, {
+                        studyId: studyRow[ 0 ],
+                    });
+
+                });
+
+        // Populate the keyword-study join table
+        }).then( function( continuation ) {
+
+            const keywords = continuation.keywords;
+            const studyId = continuation.studyId;
+
+            return Promise.all( Object.keys( keywords ).map( function( name ) {
+                return knex.insert({
+                    study_id: studyId,
+                    keyword_id: keywords[ name ],
+                }).into( 'study_keywords' );
+            }) ).then( function() {
+                return continuation;
+            });
+
+        // Populate the author-study join table
+        }).then( function( continuation ) {
+
+            const authors = continuation.authors;
+            const studyId = continuation.studyId;
+
+            return Promise.all( Object.keys( authors ).map( function( name ) {
+                return knex.insert({
+                    study_id: studyId,
+                    author_id: authors[ name ],
+                }).into( 'study_authors' );
+            }) ).then( function() {
+                return continuation;
+            });
+
+        }).then( function( continuation ) {
+
             res.json({ success: true });
+
         }).catch( function( knexError ) {
             console.error( knexError );
             res.status( 400 );
             res.json( knexError );
         });
+
+});
+
+function getAllKeywords() {
+    return knex.select( 'keywords.*' )
+        .select( knex.raw( 'COUNT( keywords.id ) as study_count' ) )
+        .from( 'keywords' )
+        .leftJoin( 'study_keywords', 'study_keywords.keyword_id', 'keywords.id' )
+        .groupBy( 'keywords.id' );
+}
+
+function getAllAuthors() {
+    return knex.select( 'authors.*' )
+        .select( knex.raw( 'COUNT( authors.id ) as study_count' ) )
+        .from( 'authors' )
+        .leftJoin( 'study_keywords', 'study_keywords.keyword_id', 'authors.id' )
+        .groupBy( 'authors.id' );
+}
+
+app.get( '/keywords', function( req, res ) {
+
+    return getAllKeywords().then( function( keywords ) {
+        res.json( keywords );
+    }).catch( function( error ) {
+        console.error( error );
+        res.status( 500 );
+        res.json( error );
+    });
+
+});
+
+app.get( '/authors', function( req, res ) {
+
+    return getAllAuthors().then( function( authors ) {
+        res.json( authors );
+    }).catch( function( error ) {
+        console.error( error );
+        res.status( 500 );
+        res.json( error );
+    });
 
 });
 
